ebf4f26028959b106800ee421e8d628d1f456ef2
Markus Frosch <markus@lazyfrosch.de>
2015-02-12 14:25:43 +0100

    Revert "Add return code to root segment"
    This reverts commit 448e883e09a8a82968cca262edb8116204b79457.

--- segments/root.py
+++ segments/root.py
@@ -6,12 +6,9 @@ def add_root_indicator_segment():
     }
     bg = Color.CMD_PASSED_BG
     fg = Color.CMD_PASSED_FG
-    extra = ""
     if powerline.args.prev_error != 0:
         fg = Color.CMD_FAILED_FG
         bg = Color.CMD_FAILED_BG
-        extra = " %d" % (powerline.args.prev_error)
-    text = extra + root_indicators[powerline.args.shell]
-    powerline.append(text, fg, bg)
+    powerline.append(root_indicators[powerline.args.shell], fg, bg)
 
 add_root_indicator_segment()
